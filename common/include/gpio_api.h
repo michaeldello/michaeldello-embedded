c5f44606c736aaad2f3fcea5418f6b03ae067057
Michael Dello <154763749+michaeldello@users.noreply.github.com>
2025-09-07 17:37:41 -0400

    Add initial documentation generation in CI (#16)
    * ME-15 add docs gen to CI
    
    ---------
    
    Co-authored-by: michaeldello <michaeldello@users.noreply.github.com>

--- common/include/gpio_api.h
+++ common/include/gpio_api.h
@@ -17,16 +17,29 @@
 #include <stdint.h>
 
 //------------------------------------------------------------------------------
+// Doxygen Brief
+//------------------------------------------------------------------------------
+
+/** @file gpio_api.h
+ *  @brief Portable GPIO API for clients to use.
+ */
+
+//------------------------------------------------------------------------------
 // API Object Definition
 //------------------------------------------------------------------------------
 
 // Define functor prototypes for the API
+/** @brief Virtual function type for GPIO pin write. */
 typedef void (*gpio_write_fn)(uint32_t pin, uint8_t value);
+/** @brief Virtual function type for GPIO pin toggle. */
 typedef void (*gpio_toggle_fn)(uint32_t pin);
 
 // Organize the GPIO API into an object type
+/** @brief Virtual function table for GPIO API. */
 typedef struct {
+    /** @brief Write a value to a GPIO pin. @return void. */
     gpio_write_fn  write;
+    /** @brief Toggle the current value on a GPIO pin. @return void. */
     gpio_toggle_fn toggle;
 }
 gpio_api_t;
@@ -37,6 +50,9 @@ extern gpio_api_t gpio;
 //------------------------------------------------------------------------------
 // Function Declarations
 //------------------------------------------------------------------------------
+/** @brief Initialize a GPIO instance: enable clock and configure pins.
+ *  @return void.
+ */
 void gpio_init(void);
 
 #endif // INCLUDE_GPIO_API_H_
